d148c805ed8f3e84789f260a9b102ec2de1f1cd9
Javier <jegj57@gmail.com>
2017-11-12 17:20:27 -0500

    Add projects template section

--- src/components/interests.js
+++ src/components/interests.js
@@ -6,10 +6,13 @@ export const Interests = ({ }) => {
 	return (
 		<ScrollableAnchor id="interests">
 			<section className="resume-section p-3 p-lg-5 d-flex flex-column">
-				<div className="my-aauto">
+				<div className="my-auto">
 					<h2 className="mb-5">Interests</h2>
 					<p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skiier and novice
 						ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
+					<p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef,
+						and I spend a large amount of my free time exploring the latest technolgy advancements in the front-end web development
+						world.</p>
 					<p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef,
 						and I spend a large amount of my free time exploring the latest technolgy advancements in the front-end web development
 						world.</p>
--- src/components/projects.js
+++ src/components/projects.js
@@ -8,6 +8,41 @@ export const Projects = ({ }) => {
 			<section className="resume-section p-3 p-lg-5 d-flex flex-column">
 				<div className="my-aauto">
 					<h2 className="mb-5">Projects</h2>
+					<div className="row">
+						<div className="col-lg-4 col-sm-6 portfolio-item">
+							<div className="card h-100">
+								<a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
+									<div className="card-body">
+										<h4 className="card-title">
+											<a href="#">Project One</a>
+										</h4>
+										<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+									</div>
+							</div>
+						</div>
+						<div className="col-lg-4 col-sm-6 portfolio-item">
+							<div className="card h-100">
+								<a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+								<div className="card-body">
+									<h4 className="card-title">
+										<a href="#">Project One</a>
+									</h4>
+									<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+								</div>
+							</div>
+						</div>
+						<div className="col-lg-4 col-sm-6 portfolio-item">
+							<div className="card h-100">
+								<a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+								<div className="card-body">
+									<h4 className="card-title">
+										<a href="#">Project One</a>
+									</h4>
+									<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</section>
 		</ScrollableAnchor>
